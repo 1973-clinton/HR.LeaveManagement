9d76899fc50cbd906008a4aa817b947922064bd9
1973-clinton <clintonboamahofficial@gmail.com>
2022-06-15 12:28:09 +0000

    Add project files.

---
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Handlers/Queries/GetLeaveTypeListRequestHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Requests.Queries;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Queries
+{
+    public class GetLeaveTypeListRequestHandler : IRequestHandler<GetLeaveTypeListRequest, List<LeaveTypeDto>>
+    {
+        //private fields
+        readonly ILeaveTypeRepository _leaveTypeRepository;
+        readonly IMapper _mapper;
+
+        //constructor
+        public GetLeaveTypeListRequestHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+        
+        //handler method
+        public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypeListRequest request, CancellationToken cancellationToken)
+        {
+            var results = await _leaveTypeRepository.GetAll();
+            return _mapper.Map<List<LeaveTypeDto>>(results);
+        }
+    }
+}
