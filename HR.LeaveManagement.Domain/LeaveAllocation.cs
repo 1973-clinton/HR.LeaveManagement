b8c76d9bff82f30addacfde410542224f53d29c9
1973-clinton <clintonboamahofficial@gmail.com>
2022-06-16 12:11:29 +0000

    changes to domain

--- HR.LeaveManagement.Domain/LeaveAllocation.cs
+++ HR.LeaveManagement.Domain/LeaveAllocation.cs
@@ -7,7 +7,7 @@ namespace HR.LeaveManagement.Domain
 {
     public class LeaveAllocation : BaseDomainEntity
     {
-        public int NumberOfDays { get; set; }       
+        public int NumberOfDays { get; set; }
         public LeaveType LeaveType { get; set; }
         public int LeaveTypeId { get; set; }
         public int Period { get; set; }
