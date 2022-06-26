deb318ba569b17f906488d3547acd1d52e9817f1
1973-clinton <clintonboamahofficial@gmail.com>
2022-06-26 14:48:19 +0000

    completed dto validations

---
+++ HR.LeaveManagement.Application/DTOs/LeaveAllocation/Validators/ILeaveAllocationDtoValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveAllocation.Validators
+{
+    public class ILeaveAllocationDtoValidator : AbstractValidator<ILeaveAllocationDto>
+    {
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+        public ILeaveAllocationDtoValidator(ILeaveAllocationRepository leaveAllocationRepository)
+        {
+            _leaveAllocationRepository = leaveAllocationRepository;
+
+            RuleFor(p => p.NumberOfDays)
+                .NotNull()
+                .GreaterThan(0).WithMessage("{PropertyValue} must be greater than {ComparisonValue}")
+                .LessThan(100).WithMessage("{PropertyValue} must be less than {ComparisonValue}");
+
+            RuleFor(p => p.Period)
+                .NotNull();
+
+            RuleFor(p => p.LeaveTypeId)
+                .GreaterThan(0)
+                .NotNull()
+                .MustAsync(async (id, token) =>
+                {
+                    var leaveTypeExists = await _leaveAllocationRepository.Exists(id);
+                    return !leaveTypeExists;
+
+                }).WithMessage("{PropertyValue} does not exist!");
+        }
+    }
+}
