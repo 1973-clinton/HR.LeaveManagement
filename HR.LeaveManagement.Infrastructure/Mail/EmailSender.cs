1146637820b09920c83d204bd463c4cbe58eaf48
kington <clintonboamahofficial@gmail.com>
2022-07-20 13:08:07 +0000

    Completed Infrastructure project

---
+++ HR.LeaveManagement.Infrastructure/Mail/EmailSender.cs
@@ -0,0 +1,36 @@
+﻿using HR.LeaveManagement.Application.Contracts.Infrastructure;
+using HR.LeaveManagement.Application.Models;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Infrastructure.Mail
+{
+    public class EmailSender : IEmailSender
+    {
+        private EmailSettings _emailSettings { get; } 
+
+        public EmailSender(IOptions<EmailSettings> emailSettings)
+        {
+            _emailSettings = emailSettings.Value;
+        }
+
+        public async Task<bool> SendEmailAsync(Email email)
+        {
+            var sendGridClient = new SendGridClient(_emailSettings.ApiKey);
+            var To = new EmailAddress(email.To);
+            var From = new EmailAddress
+            {
+                Email = _emailSettings.FromAddress,
+                Name = _emailSettings.FromName
+            };
+
+            var message = MailHelper.CreateSingleEmail(From, To, email.Subject, email.Body, email.Body);
+            var response = await sendGridClient.SendEmailAsync(message);
+
+            return response.StatusCode == System.Net.HttpStatusCode.OK || response.StatusCode == System.Net.HttpStatusCode.Accepted;
+        }
+    }
+}
---
+++ HR.LeaveManagement.Persistence/Repositories/LeaveTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Persistence.Repositories
+{
+    public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
+    {
+        
+        public LeaveTypeRepository(LeaveManagementDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
